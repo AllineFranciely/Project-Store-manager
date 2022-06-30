606a571d8821e9520fd7b4b9f47bdc44c8a7b7cc
Alline <alline_franciely@hotmail.com>
2022-06-30 10:52:54 -0300

    body parser

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 const routes = require('./routes');
 
