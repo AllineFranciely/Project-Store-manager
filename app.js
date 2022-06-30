cca80a37fee7a21558aa2a22694aeadef4abe46e
Alline <alline_franciely@hotmail.com>
2022-06-30 12:06:20 -0300

    retira o body parser

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.json());
 
 const routes = require('./routes');
 
